487e435fc586b2f49d2079f37ffca11025f81e05
ararara127 <azzahraindah127@gmail.com>
2026-04-26 20:48:17 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,26 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\ReturController;
-Route::get('/', function () {
-    return view('penjualan.dashboard');
-});
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LaporanController;
+
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::get('/index', [PenjualanController::class, 'index'])->name('penjualan.index');
 // Route::get('/tambahJualan', [PenjualanController::class, 'create'])->name('penjualan.tambahPenjualan');
 // Route::post('/tambahJualan', [PenjualanController::class, 'store'])->name('penjualan.tambahPenjualan');
 Route::resource('penjualan', PenjualanController::class);
 Route::resource('retur', ReturController::class);
+
+Route::get('/api/penjualan/{id}', function($id) {
+    return App\Models\DetailPenjualan::join('produks', 'detail_penjualans.produks_id', '=', 'produks.id')
+        ->where('penjualans_id', $id)
+        ->select('detail_penjualans.*', 'produks.nama_produk', 'produks.harga_barang')
+        ->get();
+});
+
+Route::get('/laporan/penjualan', [LaporanController::class, 'penjualan'])->name('laporan.penjualan');
+Route::get('/laporan/retur', [LaporanController::class, 'retur'])->name('laporan.retur');
 
 Route::get('/admin', function () {
     return view('admin.dashboard');
